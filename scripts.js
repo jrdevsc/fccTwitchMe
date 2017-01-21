2c13dbe15ad0839dbff6cfe1f6fb4d289d6c7418
julian rudolph <julian.rudolphsc@gmail.com>
2017-01-21 17:39:41 -0500

    final

--- scripts.js
+++ scripts.js
@@ -1,92 +1,69 @@
 var bUrl = "https://wind-bow.gomix.me/twitch-api";
 
-//premade list from fcc
+//first check if users are streaming
+//then do the new ajax calls on just those
+
 var users = ["nikolarntv", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "potsuraze", "Macie_Jay"];
+
 users.forEach(function(user){
   $.ajax({
-    url: bUrl +"/users/"+user,
-    success: function(data){
-      var bio;
+    url: bUrl+"/streams/"+user,
+    success: function(sData){
+      if(sData.stream !== null){
+        //do another ajax for this users
+        $.ajax({
+          url: bUrl+"/users/"+user,
+          success: function(uData){
+            var bio;
+            if(uData.bio == null){
+              var bio = "Sorry, User has no BIO!";
+            }else{
+              var bio = uData.bio;
+            }
 
-      if(data.bio == null){
-        bio = "User has no Bio!"
+            $('#prePop').append(`<div id='${uData.name}' class='media'><div class='media-left'><a href='#'><img class='media-object' src='${uData.logo}'></a></div><div class='media-body'><h3 class='media-heading'>${uData.name}</h3><p>${bio}</p><p>Currently Streaming: <em>${sData.stream.game}</em></p><a href='https://www.twitch.tv/${user}' class="btn btn-info">More</a></div></div>`);
+          }
+        });
       }else{
-        bio = data.bio;
+        return;
       }
-      $('#prePop').append(`<div id='${data.name}' class='media'><div class='media-left'><a href='#'><img class='media-object' src='${data.logo}'></a></div><div class='media-body'><h3 class='media-heading'>${data.name}</h3>${bio}</div></div>`);
     }
   });
 });
 
 
+//search functions
+//this is where you can check if the user deleted account
+//or if it ever existed
 
-function chkStream(user){
+$('#searchForm').on('submit', function(e){
+  e.preventDefault();
+  var search = $('#searchUser').val();
+
   $.ajax({
-    url: bUrl+"/streams/"+user,
-    success: function (sData){
-      // console.log(sData);
-
-
-      var myUser = `#${user}`;
-      // console.log(myUser);
-      if(sData.stream !== null){
-        console.log(user +" is streaming");
-        $(`#${user}`).append("yay Streaming");
+    url: bUrl+"/channels/"+search,
+    success: function(srData){
+      // console.log(srData);
+      if(srData.error){
+        alert(srData.error);
       }else{
-        console.log(user +" is not streaming");
-        $(myUser).addClass('hidden');
+        $('#results').append(`<div id='${srData.name}' class='media'><div class='media-left'><a href='#'><img class='media-object' src='${srData.logo}'></a></div><div class='media-body'><h3 class='media-heading'>${srData.name}</h3><p>${srData.status}</p><p id='streamYN'></p><a href='https://www.twitch.tv/${srData.name}' class="btn btn-info">More</a></div></div>`);
+
+
+        $.ajax({
+          url: bUrl+"/streams/"+srData.name,
+          success: function(ckData){
+            if(ckData.stream == null){
+              $('#streamYN').text(srData.name+ " is not streaming.");
+            }else{
+              console.log(srData);
+              $('#streamYN').text(srData.game);
+            }
+          }
+        });
+
       }
-      // if(sData.stream == null){
-      //   // var myUser = `#${user}`;
-      //   $(myUser).addClass('hidden');
-      //   console.log("not streaming: "+ myUser);
-      // }else{
-      //   console.log(myUser);
-      //   $(myUser).addClass('stream');
-      // }
     }
   });
-};
-// var myUser = users.forEach(function(user){
-//   //since this is a pre made list, it is going to serve as the info for the initial load
-//
-//   //make ajax call for users array
-//   $.ajax({
-//     url: bUrl+"/users/"+user,
-//     success: function(data){
-//       //populate some shit on the dom
-//       $('#prePop').append(`<div id='${data.name}' class='media notstreaming'><div class='media-left'><a href='#'><img class='media-object' src='${data.logo}'></a></div><div class='media-body'><h3 class='media-heading'>${data.name}</h3>${data.bio}</div></div>`);
-//       //call function
-//       chkStreaming(user);
-//     }
-//   });
-// });
-//
-// function chkStreaming(user){
-//   $.ajax({
-//     url: bUrl+'/streams/'+user,
-//     success: function(sData){
-//
-//
-//       if(sData.stream !== null){
-//         // console.log(user + " is streaming");
-//         isStreaming(user);
-//       }else{
-//       // console.log(user +" is not streaming");
-//       notStreaming(user);
-//     }
-//
-//
-//     }
-//   });
-// }
-//
-// function isStreaming(user){
-//   // $(`#${user}`).removeClass('notStreaming');
-//   $(`#${user} h3`).removeClass('notstreaming');
-//   $(`#${user} h3`).addClass('stream');
-// };
-//
-// function notStreaming(user){
-//   console.log(user +" not Streaming");
-// };
+
+});
